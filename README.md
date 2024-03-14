1b42f43c827247fc61d8b5b3e65b373a6d3fd9f6
sgvr <anguoyuan111@gmail.com>
2024-03-14 18:23:19 +0900

    unified.py

---
+++ README.md
@@ -0,0 +1,92 @@
+<div align="center">
+<img src="https://github.com/anguoyuan/Pixel_retrieval-Segmented_instance_retrieval/assets/91877920/6c9cc2f4-58e1-4fe6-a97c-bfb173479f16" width="200" height="100" align="middle">
+</div>
+
+
+# Official Release: Pixel Retrieval Benchmarks PRoxford and PRparis 
+
+We are pleased to announce the official release of the pixel retrieval benchmarks PRoxford and PRparis. These benchmarks are introduced in our ICCV 23 paper, "Towards Content-based Pixel Retrieval in Revisited Oxford and Paris."
+
+## Participate in Our User Study
+
+**Challenge**: Can you identify the left query object in the right ground truth image?
+
+Traditional image-level retrieval often falls short in meeting user search requirements. For a more precise and user-centric experience, pixel-level retrieval is essential.
+
+<img src="https://github.com/anguoyuan/Pixel_retrieval-Segmented_instance_retrieval/assets/91877920/dfc800bf-5548-453d-a97c-0cfa373cdad5" alt="Query Image" width="40%" align="left"> <img src="https://github.com/anguoyuan/Pixel_retrieval-Segmented_instance_retrieval/assets/91877920/ed325955-a58f-4fa1-8bd2-78bedb0abcff" alt="Ground Truth Image" width="45%" align="right">
+
+<br clear="all">
+
+Pixel retrieval isn't merely theoretical—it has tangible benefits. Our research shows that incorporating pixel-level annotations during the retrieval process significantly improves user experience.
+
+**Get Involved**: Dive into our user study and share your insights [here](https://fascinating-marzipan-a99b4c.netlify.app/bwds).
+
+
+
+## Accessing the Benchmarks
+
+### Prerequisites
+
+1. **Download Pixel-Level Labels**: Download and extract our pixel-level labels from [here](https://sgvr.kaist.ac.kr/~guoyuan/Segment_retrieval/pixel_retrieval_benchmarks.rar).
+   
+2. **Download Example Result**: We provide an example result using the 'sift sp' method. Download and extract it from [this link](https://sgvr.kaist.ac.kr/~guoyuan/Segment_retrieval/results.rar).
+
+### Evaluation Steps
+
+1. Run `miou.py` in the `evaluation_code` directory to get mIoUs for each query.
+   
+2. Run `mAP@k.py` in the `evaluation_code` directory to get the final mAP@k.
+
+### Testing Your Own Method
+
+To test your own method, named 'ABC':
+- Save the image-level and pixel-level results in `results/image-level/ABC` and `results/pixel-level/ABC/matching_result`, respectively.
+- Modify the method name in `miou.py` and `mAP@k.py`. Run them.
+
+### Directory Structure
+
+Your directory structure should be as follows:
+
+-- pixel_retrieval-Segmented_instance_retrieval
+
+|-- pixel_retrieval_benchmarks
+
+|-- evaluation_code
+
+|-- results
+
+> **Note**: We are still refining this evaluation code. If you encounter any issues while using it, please let us know. We also plan to release configurations and code to help readers replicate the results presented in our paper. Stay tuned for updates.
+
+
+
+
+
+# Content-based Pixel Retrieval
+
+Pixel retrieval, or segmented instance retrieval, is a new task within computer vision. The primary objective is to identify pixels that represent a specific query object within a database. This involves the machine's ability to recognize, localize, and segment the query object in database images in real-time. Essentially, it's a pixel-level and instance-level recognition task, as illustrated in the figure below:
+
+![Pixel Retrieval Image](https://github.com/anguoyuan/Pixel_retrieval-Segmented_instance_retrieval/assets/91877920/ae1aa937-0923-4b44-b330-b7010eb28945)
+
+## Comparison with Image Retrieval and Segmentation
+
+Pixel retrieval can be viewed as an advanced form of image retrieval, providing insights into which specific pixels correlate with the query object. When juxtaposed with semantic and instance segmentation, pixel retrieval demands a nuanced recognition capability for targets of varying granularity. For a comprehensive understanding, please refer to our [research paper](https://sgvr.kaist.ac.kr/~guoyuan/Segment_retrieval/Towards%20Content-based%20Pixel%20Retrieval%20in%20Revisited%20Oxford%20and%20Paris.pdf).
+
+
+# Introducing Pixel Retrieval Benchmarks
+
+We're excited to present the first two pixel retrieval benchmarks: **PROxford** and **PRParis**. These are built upon the renowned image retrieval datasets, ROxford and RParis. Opting for these datasets offers several advantages:
+
+1. **Relevance to Landmarks**: Landmarks are of immense business value. Consider the number of photos in your collection that feature landmarks.
+2. **Familiarity**: Utilizing well-known datasets like ROxford and RParis makes it easier for the image retrieval community to transition to this new task.
+3. **Complexity**: The task is inherently challenging. Each query can have hundreds of positive images, making it ideal for testing instance-level recognition.
+4. **Quality**: The annotations are of high caliber, facilitating comparisons between human and machine recognition performance.
+
+
+
+# Challenges in Pixel Retrieval
+
+Our comprehensive experiments highlight the inherent challenges of the pixel retrieval task. It stands apart from existing challenges, indicating that there's ample scope for research to enhance content-based pixel retrieval and, by extension, the user search experience.
+
+# Future Directions
+
+[Our insights or directions for future research or developments.]
---
+++ evaluation_code/mious.py
@@ -0,0 +1,150 @@
+# This is the code to calculate the mious of pixel retrieval task.
+# The pipeline is: matching results -> ious -> mious
+# You should save your matching results in 'Result_dir/method/matching_result', such as './results/sift_sp/matching_result/proxford.npy';
+# and then this script will calculate and save the ious and mious in 'Result_dir/method/ious' and 'Result_dir/method/mious'
+
+from gettext import npgettext
+import numpy as np
+from utils.path_manage import name_list
+import os
+import json
+from utils.iou_compute import compute_iou
+
+USING_EXISTING_IOUS=False #if True, use the existing ious, if False, compute the ious
+Result_dir='../results/'
+#Result_dir='/mnt/ssd1/guoyuan/ICCV_release/results'
+GND_folder='../pixel_retrieval_benchmarks/'
+#GND_folder='/mnt/ssd1/guoyuan/ICCV_release/pixel_retrieval_benchmarks'
+
+
+#change the method list here
+method_list=['example_sift_sp', 'delf_sp']
+#method_list=['delg_sp']
+
+for dataset in ['proxford','prparis']:
+    for method in method_list:
+        if not os.path.exists(os.path.join(Result_dir,'pixel-level',method,'ious')):
+            os.makedirs(os.path.join(Result_dir,'pixel-level',method,'ious'))
+        if not os.path.exists(os.path.join(Result_dir,'pixel-level',method,'mious')):
+            os.makedirs(os.path.join(Result_dir,'pixel-level',method,'mious'))
+
+        if USING_EXISTING_IOUS:
+            IOU_path=os.path.join(Result_dir,'pixel-level',method,'ious', dataset+'_ious.npy')
+            IOU_dict=np.load(IOU_path,allow_pickle=True).item()
+        else:
+            IOU_dict={}
+
+            matching_file=dataset+'.npy'
+            matching_file=os.path.join(Result_dir,'pixel-level',method, 'matching_result' ,matching_file)
+            match_set=np.load(matching_file, allow_pickle=True).item()
+
+            gnd_seg_dict_path=os.path.join(GND_folder,dataset[2:]+'_pair2labelpath.npy')
+            gnd_seg_dict=np.load(gnd_seg_dict_path,allow_pickle=True).item()
+
+        query_list, index_list,gnd=name_list(dataset)  
+
+        medium_bbox_mious=[]
+        medium_seg_mious=[]
+        hard_bbox_mious=[]
+        hard_seg_mious=[]
+        for q in range(70):
+            q_medium_bbox_ious=[]
+            q_medium_seg_ious=[]
+            q_hard_bbox_ious=[]
+            q_hard_seg_ious=[]
+            for i in gnd[q]['hard']:
+
+                if USING_EXISTING_IOUS:
+                    bbox_iou,segmentation_iou=IOU_dict[str(q)+'_'+str(i)]
+                else:
+                    found_points=match_set['qimlist_'+str(q)+'_imlist_'+str(i)]
+                    if len(found_points) == 0:
+                        #bbox_seg_ious[str(q)+'_'+str(i)]=[0,0]
+                        bbox_iou=0
+                        segmentation_iou=0
+                        IOU_dict[str(q)+'_'+str(i)]=[bbox_iou,segmentation_iou]
+
+                        q_medium_bbox_ious.append(bbox_iou)
+                        q_medium_seg_ious.append(segmentation_iou)
+                        q_hard_bbox_ious.append(bbox_iou)
+                        q_hard_seg_ious.append(segmentation_iou)
+                        continue
+
+                    gnd_seg_path=gnd_seg_dict[str(q)+'_'+str(i)]
+                    gnd_seg_path = os.path.join(GND_folder, gnd_seg_path)
+                    gt_label = json.load(open(gnd_seg_path))
+                    #gt_points = gt_label['shapes'][0]['points']
+                    gt_points = []
+                    for shape in gt_label['shapes']:
+                        gt_points.append(shape['points'])
+
+                    try:
+                        bbox_iou= compute_iou(found_points, gt_points, type='bbox')
+                        segmentation_iou= compute_iou(found_points, gt_points,  type='segmentation')
+                        IOU_dict[str(q)+'_'+str(i)]=[bbox_iou,segmentation_iou]
+                    except:
+                        print('error pair: ', str(q)+'_'+str(i))
+
+                q_medium_bbox_ious.append(bbox_iou)
+                q_medium_seg_ious.append(segmentation_iou)
+                q_hard_bbox_ious.append(bbox_iou)
+                q_hard_seg_ious.append(segmentation_iou)
+
+            
+            for i in gnd[q]['easy']:
+                if USING_EXISTING_IOUS:
+                    bbox_iou,segmentation_iou=IOU_dict[str(q)+'_'+str(i)]
+                else:
+                    found_points=match_set['qimlist_'+str(q)+'_imlist_'+str(i)]
+                    if len(found_points) == 0:
+                        #bbox_seg_ious[str(q)+'_'+str(i)]=[0,0]
+                        bbox_iou=0
+                        segmentation_iou=0
+                        continue
+
+                    gnd_seg_path=gnd_seg_dict[str(q)+'_'+str(i)]
+                    gnd_seg_path = os.path.join(GND_folder, gnd_seg_path)
+                    gt_label = json.load(open(gnd_seg_path))
+                    #gt_points = gt_label['shapes'][0]['points']
+                    gt_points = []
+                    for shape in gt_label['shapes']:
+                        gt_points.append(shape['points'])
+
+                    try:
+                        bbox_iou= compute_iou(found_points, gt_points, type='bbox')
+                        segmentation_iou= compute_iou(found_points, gt_points,  type='segmentation')
+                        IOU_dict[str(q)+'_'+str(i)]=[bbox_iou,segmentation_iou]
+                    except:
+                        print('error pair: ', str(q)+'_'+str(i))
+
+                q_medium_bbox_ious.append(bbox_iou)
+                q_medium_seg_ious.append(segmentation_iou)
+
+            medium_bbox_mious.append(np.mean(q_medium_bbox_ious))
+            medium_seg_mious.append(np.mean(q_medium_seg_ious))
+            hard_bbox_mious.append(np.mean(q_hard_bbox_ious))
+            hard_seg_mious.append(np.mean(q_hard_seg_ious))
+            print(dataset,'query ', q, 'results are: ',np.mean(q_medium_bbox_ious),np.mean(q_medium_seg_ious), np.mean(q_hard_bbox_ious),np.mean(q_hard_seg_ious))
+
+        medium_bbox_mmiou=np.mean(medium_bbox_mious)
+        medium_seg_mmiou=np.mean(medium_seg_mious)
+        hard_bbox_mmiou=np.mean(hard_bbox_mious)
+        hard_seg_mmiou=np.mean(hard_seg_mious)
+        print(method, dataset)
+        print('medium_bbox_mmiou is: ',medium_bbox_mmiou )
+        print('medium_seg_mmiou is: ',medium_seg_mmiou )
+        print('hard_bbox_mmiou is: ',hard_bbox_mmiou )
+        print('hard_seg_mmiou is: ',hard_seg_mmiou )
+
+        result={'mmiou':[medium_bbox_mmiou,medium_seg_mmiou,hard_bbox_mmiou,hard_seg_mmiou],
+        'mious':[medium_bbox_mious,medium_seg_mious,hard_bbox_mious,hard_seg_mious],
+        'explain':'mmiou and mious in the order of [medium_bbox_,medium_seg_,hard_bbox_,hard_seg_]'
+        }
+
+        #save the result
+        np.save(os.path.join(Result_dir,'pixel-level',method,'mious',dataset+'.npy'), result)
+        #save ious
+        if not USING_EXISTING_IOUS:
+            np.save(os.path.join(Result_dir,'pixel-level',method,'ious',dataset+'_ious.npy'), IOU_dict)
+
+
